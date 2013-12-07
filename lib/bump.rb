33ebd13a06f338e2eae173fa105a6f7d7f75f917
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-07 21:26:41 +0900

    Create VersionBumpService

--- lib/bump.rb
+++ lib/bump.rb
@@ -52,8 +52,6 @@ module Bump
             @pattern = pattern || PLACEHOLDER_PATTERN # default pattern is '%.%.%'
             @before_version = before_version
             @after_version = after_version
-
-            prepare
         end
 
         def prepare
@@ -62,6 +60,8 @@ module Bump
         end
 
         def perform
+            prepare
+
             contents = File.read @file, :encoding => Encoding::UTF_8
             File.write @file, contents.sub(@before_pattern, @after_pattern)
         end
@@ -84,46 +84,104 @@ module Bump
         end
     end
 
+    class VersionBumpService
+
+        def initialize(config, file)
+            @config = config
+            @file = file
+
+            @version = Version.createFromString @config['version']
+
+            @before_version = @version.to_s
+            @after_version = @version.to_s
+
+
+        end
+
+        def resetAfterVersion
+            @after_version = @version.to_s
+            @config['version'] = @after_version
+        end
+
+        def self.createFromFile file
+            config = YAML.load_file file
+            self.new config, file
+        end
+
+        def saveConfig
+            File.write @file, config.to_yaml
+        end
+
+        def majorBump
+            @version.bump 'major'
+
+            resetAfterVersion
+        end
+
+        def minorBump
+            @version.bump 'minor'
+
+            resetAfterVersion
+        end
+
+        def patchBump
+            @version.bump 'patch'
+
+            resetAfterVersion
+        end
+
+        def config
+            @config
+        end
+
+        def performFileRewrite
+            @rewrite_rules = config['files'].map { |file, param|
+                FileRewriteRuleFactory.create(file, param, @before_version, @after_version)
+            }.flatten
+
+            p @rewrite_rules
+
+            @rewrite_rules.each do |rewrite_rule|
+                rewrite_rule.perform
+            end
+        end
+
+        def afterVersion
+            @after_version
+        end
+
+        def beforeVersion
+            @before_version
+        end
+
+    end
+
     class CLI
 
         VERSION_FILE = '.version'
 
         def main
 
-            config = YAML.load_file VERSION_FILE
+            srv = VersionBumpService.createFromFile VERSION_FILE
 
-            puts config
-
-            version = Version.createFromString config['version']
-
-            p before_version = version.to_s()
+            p srv.config
 
             opt = OptionParser.new
 
             commit_file = false
 
-            opt.on('-l') { |x| version.bump 'major' }
-            opt.on('-m') { |x| version.bump 'minor' }
-            opt.on('-s') { |x| version.bump 'patch' }
+            opt.on('-l') { |x| srv.majorBump }
+            opt.on('-m') { |x| srv.minorBump }
+            opt.on('-s') { |x| srv.patchBump }
             opt.on('-f') { |x| commit_file = true }
 
             opt.parse!(ARGV)
 
-            p after_version = version.to_s()
+            p srv.config
 
-            p config['files'].map { |file, param|
-                FileRewriteRuleFactory.create(file, param, before_version, after_version)
+            srv.performFileRewrite
 
-            }.flatten.each { |rewriteRule|
-                rewriteRule.perform
-
-            }
-
-            config['version'] = after_version
-
-            puts config
-
-            File.write VERSION_FILE, config.to_yaml
+            srv.saveConfig
 
             if commit_file
                 puts `git add . ; git commit -m "Bump to version v#{after_version}"`
