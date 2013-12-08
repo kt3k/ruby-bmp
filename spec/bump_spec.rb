01ed6fb47170dd77eee3d549c9a28f5c3a71de5c
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-08 23:42:18 +0900

    Add placeholder test

--- spec/bump_spec.rb
+++ spec/bump_spec.rb
@@ -1,9 +1,25 @@
 # bump_spec.rb
 
+require 'bump'
+
 describe Bump do
 
     it "exists" do
         Bump
+    end
+
+    describe Bump::Version do
+
+        describe "#initialize" do
+
+            it "constructs" do
+
+                expect(Bump::Version.new('1', '2', '3')).not_to be_nil
+
+            end
+
+        end
+
     end
 
 end
