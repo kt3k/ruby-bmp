75e6e0ebe06f5592a459c5d003a0b81e539528fb
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-12 00:04:28 +0900

    Add version factory tests

--- spec/bump/domain/version_factory_spec.rb
+++ spec/bump/domain/version_factory_spec.rb
@@ -2,4 +2,32 @@
 
 describe Bump::VersionFactory do
 
+    describe "#fromString" do
+
+        it "creates version object from version string" do
+
+            version = Bump::VersionFactory.fromString "1.2.3"
+
+            expect(version.to_s).to eq "1.2.3"
+
+            version.patchBump
+
+            expect(version.to_s).to eq "1.2.4"
+
+            version.minorBump
+
+            expect(version.to_s).to eq "1.3.0"
+
+            version.majorBump
+
+            expect(version.to_s).to eq "2.0.0"
+
+            version.append 'rc1'
+
+            expect(version.to_s).to eq "2.0.0rc1"
+
+        end
+
+    end
+
 end
