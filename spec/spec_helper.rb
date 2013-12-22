8b0ff693273bb0ce8d42b8143521b90887645af4
Yoshiya Hinosawa <stibium121@gmail.com>
2013-12-22 10:54:50 +0900

    Add coveralls formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
 
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
 
 SimpleCov.start do
     add_filter '/spec/'
 end
-
-#require 'coveralls'
 
 #Coveralls.wear!
