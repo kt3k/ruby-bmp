aa3d8284a4ad51a36c244fad1c05e474b06d4aaa
Yoshiya Hinosawa <stibium121@gmail.com>
2014-03-23 14:17:29 +0900

    Change version descriptor's name

--- lib/bump/cli.rb
+++ lib/bump/cli.rb
@@ -7,20 +7,20 @@ module Bump
 
     class CLI
 
-        VERSION_FILE = '.version'
+        VERSION_FILE = '.bmp.yml'
 
         CLI_NAME = 'bmp'
 
         def main
 
             opts = Slop.parse do
-                banner "Usage: #{CLI_NAME} [-p|-m|-j] [-f]"
+                banner "Usage: #{CLI_NAME} [-p|-m|-j] [-c]"
 
                 on :i, :info, 'show current version info'
                 on :p, :patch, 'bump patch (0.0.1) level'
                 on :m, :minor, 'bump minor (0.1.0) level'
                 on :j, :major, 'bump major (1.0.0) level'
-                on :f, :fix, 'fix bumping and commit current changes (git required)'
+                on :c, :commit, 'commit bump changes (git required)'
                 on :h, :help, 'show this help and exit'
                 on :v, :version, 'show version and exit'
             end
