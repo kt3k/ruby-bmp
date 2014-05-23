beb26f3e85ba0c287684bd73a367a0ef10be77d3
Yoshiya Hinosawa <stibium121@gmail.com>
2014-05-23 17:26:57 +0900

    Add FileRewriteRule tests

--- spec/bump/domain/file_rewrite_rule_spec.rb
+++ spec/bump/domain/file_rewrite_rule_spec.rb
@@ -4,4 +4,38 @@ require 'spec_helper'
 
 describe Bump::FileRewriteRule do
 
+    describe '#file' do
+
+        it 'returns file property' do
+            rule = Bump::FileRewriteRule.new 'abc', '%.%.%', '0.0.1', '1.0.0'
+
+            expect(rule.file).to eq 'abc'
+        end
+
+    end
+
+    describe '#beforePattern' do
+
+        it 'returns before_pattern property' do
+            rule = Bump::FileRewriteRule.new 'abc', 'v%.%.%', '0.0.1', '1.0.0'
+
+            rule.prepare
+
+            expect(rule.beforePattern).to eq 'v0.0.1'
+        end
+
+    end
+
+    describe '#afterPattern' do
+
+        it 'returns before_pattern property' do
+            rule = Bump::FileRewriteRule.new 'abc', 'v%.%.%', '0.0.1', '1.0.0'
+
+            rule.prepare
+
+            expect(rule.afterPattern).to eq 'v1.0.0'
+        end
+
+    end
+
 end
