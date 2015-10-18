9aec4a795074dee8e4b496859858511ba8e547a3
Yoshiya Hinosawa <stibium121@gmail.com>
2015-10-18 14:58:58 +0900

    Add command_spec

---
+++ spec/bump/command_spec.rb
@@ -0,0 +1,23 @@
+
+
+
+describe Bump::Command do
+
+    describe '#exec' do
+
+        it 'prints and executes the command' do
+
+            logger = double()
+
+            comm = Bump::Command.new logger
+
+            expect(logger).to receive(:log).with "===> echo 1"
+            expect(logger).to receive(:log).with "1\n"
+
+            comm.exec "echo 1"
+
+        end
+
+    end
+
+end
