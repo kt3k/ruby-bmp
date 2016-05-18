cca1338c8053386f1aaa66a22b9e5e52a546bd61
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-18 21:12:54 +0900

    refactor(application.rb): reduce complexity

--- spec/bump/application_spec.rb
+++ spec/bump/application_spec.rb
@@ -14,48 +14,48 @@ describe Bump::Application do
         allow(@logger).to receive(:red)
     end
 
-    describe 'selectAction' do
+    describe '#select_action' do
         it 'returns :help if the options contain :help' do
             app = Bump::Application.new({ help: true }, @help_message, @version_exp, @bmp_file, @logger)
 
-            expect(app.selectAction).to eq :help
+            expect(app.select_action).to eq :help
         end
 
         it 'returns :version if the :version option preset' do
             app = Bump::Application.new({ version: true }, @help_message, @version_exp, @bmp_file, @logger)
 
-            expect(app.selectAction).to eq :version
+            expect(app.select_action).to eq :version
         end
 
         it 'returns :info if the :info option present' do
             app = Bump::Application.new({ info: true }, @help_message, @version_exp, @bmp_file, @logger)
 
-            expect(app.selectAction).to eq :info
+            expect(app.select_action).to eq :info
         end
 
         it 'returns :bump if one of :major, :minor, :patch, :commit, :preid, :release options present' do
             app = Bump::Application.new({ major: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
 
             app = Bump::Application.new({ minor: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
 
             app = Bump::Application.new({ patch: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
 
             app = Bump::Application.new({ commit: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
 
             app = Bump::Application.new({ preid: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
 
             app = Bump::Application.new({ release: true }, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :bump
+            expect(app.select_action).to eq :bump
         end
 
         it 'returns :info if none of the above options are present' do
             app = Bump::Application.new({}, @help_message, @version_exp, @bmp_file, @logger)
-            expect(app.selectAction).to eq :info
+            expect(app.select_action).to eq :info
         end
     end
 
